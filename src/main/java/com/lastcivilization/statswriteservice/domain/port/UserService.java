78a5a93229c9df2bf6eba0bbe46c304e6f28aa7c
mrrevo55 <kwolny31@gmail.com>
2022-09-22 11:44:18 +0200

    Optimized imports

--- src/main/java/com/lastcivilization/statswriteservice/domain/port/UserService.java
+++ src/main/java/com/lastcivilization/statswriteservice/domain/port/UserService.java
@@ -1,7 +1,6 @@
 package com.lastcivilization.statswriteservice.domain.port;
 
 import com.lastcivilization.statswriteservice.domain.dto.UserDto;
-import com.lastcivilization.statswriteservice.domain.exception.NotEnoughMoneyException;
 
 public interface UserService {
 
--- src/main/java/com/lastcivilization/statswriteservice/infrastructure/database/EntityMapper.java
+++ src/main/java/com/lastcivilization/statswriteservice/infrastructure/database/EntityMapper.java
@@ -1,11 +1,7 @@
 package com.lastcivilization.statswriteservice.infrastructure.database;
 
-import com.lastcivilization.statswriteservice.domain.dto.LvlDto;
 import com.lastcivilization.statswriteservice.domain.dto.StatsDto;
-import com.lastcivilization.statswriteservice.domain.dto.StatsValueDto;
-import com.lastcivilization.statswriteservice.domain.dto.TimeBonusDto;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
